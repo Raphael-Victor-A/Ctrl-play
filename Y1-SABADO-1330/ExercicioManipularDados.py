cc1def179ab8278f3ce3d6e0344310238d79e828
Raphael-Victor-A <114350662+Raphael-Victor-A@users.noreply.github.com>
2024-11-23 15:17:59 -0400

    Manipular dados

---
+++ Y1-SABADO-1330/ExercicioManipularDados.py
@@ -0,0 +1,6 @@
+n1 = float(input("Qual a primeira nota: ")) # recebimento de um numero inteiro
+n2 = float(input("Qual a segunda nota")) # recebimento de um numero flutuante
+
+media = (n1+n2)/2
+
+print("O usuario teve como media: ", media)
